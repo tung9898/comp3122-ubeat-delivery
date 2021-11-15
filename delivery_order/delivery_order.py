b8855dd9e28c1f213e32cda3e825836a954aef09
tung9898 <hoyiutung@hotmail.com>
2021-11-15 21:28:10 +0800

    [patch] Add redis pubsub() subscribe on port 6379

--- delivery_order/delivery_order.py
+++ delivery_order/delivery_order.py
@@ -7,6 +7,7 @@ from pymongo import MongoClient
 
 flask_app = flask.Flask(__name__)
 deliveriesdb = MongoClient('mongodb://comp3122:23456@delivery_order_db:27017')
+redis_conn = redis.Redis(host='message_queue', port=6379)
 
 ################
 # Redis events
@@ -48,5 +49,9 @@ def get_order(order_id):
 ##############################
 #       
 if __name__ == '__main__':
+    redis_pubsub = redis_conn.pubsub()
+    redis_pubsub.subscribe(**{'deliveryOrder_setTaken': set_taken})
+    redis_pubsub.subscribe(**{'deliveryOrder_addShipped': add_shipped})
+    redis_pubsub_thread = redis_pubsub.run_in_thread(sleep_time=0.001)
     flask_app.run(host='0.0.0.0', debug=True, port=15000)
 
