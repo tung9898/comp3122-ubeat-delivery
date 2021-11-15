1fdb67bf8b1e24c49531dd1475bd9613946498c4
tung9898 <hoyiutung@hotmail.com>
2021-11-15 21:24:28 +0800

    [feature] Add add_shipped function

--- delivery_order/delivery_order.py
+++ delivery_order/delivery_order.py
@@ -8,6 +8,20 @@ from pymongo import MongoClient
 flask_app = flask.Flask(__name__)
 deliveriesdb = MongoClient('mongodb://comp3122:23456@delivery_order_db:27017')
 
+################
+# Redis events
+################
+
+def add_shipped(message):
+    load = json.loads(message['data'])
+    delivery = deliveriesdb.delivery_orders.deliveries.find_one({'delivery_id': load['delivery_id']})
+    delivery['orders'].append({'order_id': load['order_id'], 'customer_id': load['customer_id'], 'restaurant_id': load['restaurant_id'], 'taken': 0})
+    deliveriesdb.delivery_orders.deliveries.replace_one({'_id': delivery['_id']}, delivery)
+
+###################
+# Flask endpoints
+###################
+
 @flask_app.route('/<order_id>', methods=['GET'])
 def get_order(order_id):
     orders = deliveriesdb.delivery_orders.deliveries.find_one({'orders.order_id': order_id}, { '_id': 0})
@@ -17,7 +31,11 @@ def get_order(order_id):
     for order in orders:
         if order['order_id'] == order_id:
             return order, 200
-            
+
+##############################
+# Main: Run flask, establish subscription
+##############################
+#       
 if __name__ == '__main__':
     flask_app.run(host='0.0.0.0', debug=True, port=15000)
 
--- delivery_order/mongo-init.js
+++ delivery_order/mongo-init.js
@@ -1,3 +1,30 @@
 db.auth('comp3122', '23456')
 db = db.getSiblingDB('delivery_orders')
+
 db.createCollection('deliveries')
+db.deliveries.insertOne({
+    'delivery_id':1,
+    'name': 'daniel',
+    'orders':[
+        {'order_id':'r1o1','customer_id':1,'restaurant_id':1,'taken':1},
+        {'order_id':'r2o2','customer_id':2,'restaurant_id':2,'taken':1}
+    ]
+})
+
+db.deliveries.insertOne({
+    'delivery_id':2,
+    'name': 'daisy',
+    'orders':[
+        {'order_id':'r3o1', 'customer_id':2,'restaurant_id':3,'taken':1},
+        {'order_id':'r3o2', 'customer_id':3,'restaurant_id':3,'taken':1}
+    ]
+})
+
+db.deliveries.insertOne({
+    'delivery_id':3,
+    'name': 'dylan',
+    'orders':[
+        {'order_id':'r2o1', 'customer_id':1,'restaurant_id':2,'taken':1},
+        {'order_id':'r1o2', 'customer_id':3,'restaurant_id':1,'taken':0}
+    ]
+})
